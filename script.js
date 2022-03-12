653b87318fa1d3021e7ef1a97686f2780790e671
Fraz Aslam <faslam7646@gmail.com>
2022-03-12 14:13:03 -0500

    Created files, created working code for a random selection for AI

---
+++ script.js
@@ -0,0 +1,17 @@
+let computerOptions = [
+    "Rock",
+    "Paper",
+    "Scissors"
+];
+
+let computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];   
+
+function computerPlay(comptuerChoice){  
+}
+
+console.log(computerChoice);
+    
+    
+
+
+
